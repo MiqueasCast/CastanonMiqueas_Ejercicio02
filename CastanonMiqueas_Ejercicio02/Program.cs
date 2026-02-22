5c4a8f7245ec4c92b9cb4803b1e264c36cb2b591
MiqueasCast <castmicah@gmail.com>
2026-02-21 23:56:10 -0600

    Integración de menu

--- CastanonMiqueas_Ejercicio02/Program.cs
+++ CastanonMiqueas_Ejercicio02/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Bienvenido al menu de inicio \n selecciones la opccion que desee \n1 Estudiante \n2.Docente \n3 Técnico IT \n4 Visitante ");
+int options = int.Parse(Console.ReadLine());
+Console.WriteLine("ingrese hora en formano 24");
+int time = int.Parse(Console.ReadLine());
+Console.WriteLine("ingrese dia de la semana");
+int day = int.Parse(Console.ReadLine());
+Console.WriteLine("ingrese carnet vigente s/n");
+string carnet = Console.ReadLine();
+Console.WriteLine("ingrese autorizacion escrita s/n");
+string autorization = Console.ReadLine();
+Console.WriteLine("trae usb s/n");
+string usb = Console.ReadLine();
+Console.WriteLine("Si trae USB preguntar si está acompañado S/N");
+string partner = Console.ReadLine();
+
